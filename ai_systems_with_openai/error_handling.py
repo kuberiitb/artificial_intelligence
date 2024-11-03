fdf0e339e977220854def93fae4d0c5774f5b613
Kuber <5146792+kuberiitb@users.noreply.github.com>
2024-11-03 19:21:20 +0530

    Create error_handling.py

---
+++ ai_systems_with_openai/error_handling.py
@@ -0,0 +1,13 @@
+# erros in openai API library -- 404 - model doesn't exists
+
+# connection errors
+- InternalServerError
+- APIConnectionError
+- APITimeoutError
+Solution- Check connection config
+
+Reduce limitis error
+- Conflict error, RateLimitError
+Solution: check limit restrictions, ensure rates are in limit.
+
+# Authentication error - key expired   
